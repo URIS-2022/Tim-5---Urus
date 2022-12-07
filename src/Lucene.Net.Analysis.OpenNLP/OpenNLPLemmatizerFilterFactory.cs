2af8aebea5f465741f63022464b673ed817d1508
AzureAD\AndrejKatin <katin.andrej96@gmail.com>
2022-12-07 15:03:13 +0100

    Test comment 2

--- src/Lucene.Net.Analysis.OpenNLP/OpenNLPLemmatizerFilterFactory.cs
+++ src/Lucene.Net.Analysis.OpenNLP/OpenNLPLemmatizerFilterFactory.cs
@@ -61,6 +61,7 @@ namespace Lucene.Net.Analysis.OpenNlp
             }
 
             // Comment 1
+            // Comment 2
             if (args.Count > 0)
             {
                 throw new ArgumentException(string.Format(J2N.Text.StringFormatter.CurrentCulture, "Unknown parameters: {0}", args));
